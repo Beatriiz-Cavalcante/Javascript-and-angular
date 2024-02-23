5b725c95195ea15266c02bd6a294fce1ee1d8f8c
Beatriiz_Cavalcante <biamenezes193@gmail.com>
2024-02-23 12:57:23 -0300

    Criando componentes com angular

---
+++ angular01/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu.component';
+import { ButtonComponent } from './componentes/button.component';
+
+
+@NgModule({
+  declarations: [
+    [AppComponent, MenuComponent, ButtonComponent ]
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
